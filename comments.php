19a16464c6a79657015630b0f3c0c7f6c994df42
Lakshay Verma <lakshay.verma@walkwel.in>
2017-12-04 09:10:05 +0530

    Chapter 04-01 begin

--- comments.php
+++ comments.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The template for displaying comments.
  *
@@ -16,7 +15,7 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if (post_password_required()) {
+if ( post_password_required() ) {
 	return;
 }
 ?>
@@ -24,62 +23,63 @@ if (post_password_required()) {
 <div id="comments" class="comments-area">
 
 	<?php
-    // You can start editing here -- including this comment!
-if (have_comments()) : ?>
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-		printf( // WPCS: XSS OK.
-			esc_html(_nx('One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'humescores')),
-			number_format_i18n(get_comments_number()),
-			'<span>' . get_the_title() . '</span>'
-		);
-		?>
+				printf( // WPCS: XSS OK.
+					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'humescores' ) ),
+					number_format_i18n( get_comments_number() ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			?>
 		</h2>
 
-		<?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : // Are there comments to navigate through??>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e('Comment navigation', 'humescores'); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'humescores' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link(esc_html__('Older Comments', 'humescores')); ?></div>
-				<div class="nav-next"><?php next_comments_link(esc_html__('Newer Comments', 'humescores')); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'humescores' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'humescores' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation.?>
+		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
-		wp_list_comments([
-			'style' => 'ol',
-			'short_ping' => true,
-		]);
-		?>
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+				) );
+			?>
 		</ol><!-- .comment-list -->
 
-		<?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : // Are there comments to navigate through??>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e('Comment navigation', 'humescores'); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'humescores' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link(esc_html__('Older Comments', 'humescores')); ?></div>
-				<div class="nav-next"><?php next_comments_link(esc_html__('Newer Comments', 'humescores')); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'humescores' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'humescores' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
 		<?php
-	endif; // Check for comment navigation.
+		endif; // Check for comment navigation.
 
 	endif; // Check for have_comments().
 
-    // If comments are closed and there are comments, let's leave a little note, shall we?
-	if (!comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments')) : ?>
 
-		<p class="no-comments"><?php esc_html_e('Comments are closed.', 'humescores'); ?></p>
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'humescores' ); ?></p>
 	<?php
-endif;
+	endif;
 
-comment_form();
-?>
+	comment_form();
+	?>
 
 </div><!-- #comments -->
